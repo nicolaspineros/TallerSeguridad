84cbc62403a28fb297c792b2c2da64c468b71ea2
Juano <juanosierra2@gmail.com>
2022-07-07 09:36:07 -0500

    Login

---
+++ src/main/java/co/edu/escuelaing/securityprimerlive/Securityprimerlive.java
@@ -0,0 +1,14 @@
+package co.edu.escuelaing.securityprimerlive;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Securityprimerlive {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        SpringApplication.run(Securityprimerlive.class, args);
+    }
+}
